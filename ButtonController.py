c8daf39b720000024dfae46f5c11385c9e3e4d91
Mafkees35 <34511154+Mafkees35@users.noreply.github.com>
2018-03-14 11:28:29 +0100

    input delay
     om fouten met de knoppen te voorkomen

--- ButtonController.py
+++ ButtonController.py
@@ -7,6 +7,7 @@ Auteurs: Matthijs Daggelders
 
 import UIController
 import RPi.GPIO as GPIO
+from PyQt5 import QtTest
 
 
 class ButtonController(object):
@@ -65,6 +66,7 @@ class ButtonController(object):
                     if self.DEBUG: print(self.mcp.get_name, "port: ", port, "bit: ", i+1)
                     self.output(port, i)
         self.currVal = value
+        QtTest.QTest.qWait(200)
         return
 
     def output(self, port, bit):
